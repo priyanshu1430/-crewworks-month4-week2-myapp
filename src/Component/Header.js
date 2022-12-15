2ee913c54bbb7100f777a2a5ecf583146acc1253
priyanshu1430 <priyanshugpt143@gmail.com>
2022-12-15 22:07:14 +0530

    done

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Style.css";
 
 function Header() {
   return (
-    <div><h1>header</h1></div>
+    <div>
+      <span className='header'> React Context API Tutorial</span>
+      <ul className='nav'>
+        <li>
+          <Link to='/'>Home Page</Link>
+        </li>
+        <li >
+          <Link to= "/cart"></Link>
+        </li>
+      </ul>
+  
+    </div>
   )
 }
 
